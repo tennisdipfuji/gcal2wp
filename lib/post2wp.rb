a63cff928264420e757e0aace9858656e5a0bdcd
Kohei MATSUSHITA <ma2shita+git@ma2shita.jp>
2014-05-30 15:06:55 +0900

    "Module"ly Post2Wp.

--- lib/post2wp.rb
+++ lib/post2wp.rb
@@ -1,20 +1,22 @@
 require 'net/http'
 require 'uri'
-require 'date'
-require './secret_keys'
+require 'time'
+require "#{File.dirname(__FILE__)}/../secret_keys"
 
-token_type = 'bearer'
-site = 'tennisdipfuji.wordpress.com'
-post_id = '98'
-bd = {content: STDIN.read, date: DateTime.now.iso8601}
+module Post2Wp
+  TOKEN_TYPE = 'bearer'
+  SITE = 'tennisdipfuji.wordpress.com'
 
-uri = URI.parse("https://public-api.wordpress.com/rest/v1/sites/#{site}/posts/#{post_id}?pretty=1")
-req = Net::HTTP::Post.new(uri.request_uri)
-req['authorization'] = "#{token_type} #{ACCESS_TOKEN}"
-req.set_form_data(bd)
-http = Net::HTTP.new(uri.host, uri.port)
-http.use_ssl = true
-#http.set_debug_output STDERR
-res = http.start{|h| h.request(req)}
-STDERR.puts res.body
-
+  def self.run(post_content, post_id = 98, post_date = Time.now)
+    bd = {content: post_content, date: post_date.iso8601}
+    uri = URI.parse("https://public-api.wordpress.com/rest/v1/sites/#{SITE}/posts/#{post_id}?pretty=1")
+    req = Net::HTTP::Post.new(uri.request_uri)
+    req['authorization'] = "#{TOKEN_TYPE} #{ACCESS_TOKEN}"
+    req.set_form_data(bd)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    #http.set_debug_output STDERR
+    res = http.start{|h| h.request(req)}
+    res.body
+  end
+end
